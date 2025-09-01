fc85f2839e2ef29fe8b36f61508a0876bd16a38c
itsabunny <itsabunny@pm.me>
2025-09-02 00:19:16 +0200

    Add AboutPage component and corresponding CSS styles

---
+++ src/AboutPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import "./AboutPage.css";
+
+const AboutPage = () => {
+  return (
+    <div className='about-container'>About Page
+        <h1>hi</h1>
+    </div>
+  )
+}
+
+export default AboutPage
