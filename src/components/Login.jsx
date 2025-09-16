3e16efecab3ef4752fd01d0a7b86ed2e8a3a4700
eckofox1981 <eckofox1981@pm.me>
2025-09-16 09:34:02 +0200

    removed link to user creation

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -39,23 +39,6 @@ export function Login({
             />
             <button type="submit">Login</button>
           </form>
-          <div className="auth-links">
-            <p onClick={() => setView("register")}>Create new user</p>
-          </div>
-        </>
-      )}
-
-      {view === "register" && (
-        <>
-          <h2>Create New User</h2>
-          <form className="auth-form">
-            <input type="text" placeholder="Username" required />
-            <input type="password" placeholder="Password" required />
-            <button type="submit">Register</button>
-          </form>
-          <div className="auth-links">
-            <p onClick={() => setView("login")}>Back to login</p>
-          </div>
         </>
       )}
     </div>
